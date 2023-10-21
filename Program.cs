602a008336b5aca8b50ae452d79473ce1507f75e
CHAD-DEV\Medium Chad <cbrathwaite93@gmail.com>
2023-10-21 19:02:40 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+class addnsubtract
+{
+    static void Main()
+    {
+        Console.WriteLine("Would you like to Add(A) or Subtract(S)?");
+        string letter = Console.ReadLine();
+        bool isA = IsletterA(letter);
+        bool isS = IsLetterS(letter);
+
+        if(isA)
+        {
+            Console.WriteLine("Enter the first number:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int result = num1 + num2;
+            Console.WriteLine($"The result of {num1} + {num2} is: {result}");
+        }
+        else if (isS)
+        {
+            Console.WriteLine("Enter the first number:");
+            int num1 = Convert.ToInt32((string) Console.ReadLine());
+
+            Console.WriteLine("Enter the second number:");
+            int num2 = Convert.ToInt32((string) Console.ReadLine());
+
+            int result = num1 - num2;
+            Console.WriteLine($"The result of {num1} - {num2} is; {result}");
+        }
+        else 
+        { Console.WriteLine("Yeah.... Just go ahead and turn off your compter");
+        }
+
+        static bool IsletterA(string letter)
+        {
+            return letter.ToLower() == "a";
+        }
+        static bool IsLetterS(string letter)
+        {
+            return letter.ToLower() == "s";
+        }
+        
+    }
+}
